6c6026f5d3f153e0f9da5130d4c8f1cb7743c083
Mohammed-Fahmy99 <mohammed.fahmy.mail@gmail.com>
2023-07-10 13:59:02 +0300

    fix: regex in routing system

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,35 @@
-// import { createServer } from "http";
-
-// const routes: any = [];
-
-// function addNewRoute(route: string, callback: () => void): void {
-//     let regEx = "/";
-//     const routeChunks = route.split('/').filter(item => item !== '')    // /users/{user_id} => ['users', '{user_id}'] => /\/users\/w+/
-//     for (const chunk of routeChunks) {
-//         regEx += '\/'
-//         if (chunk.startsWith('{') && chunk.endsWith('}')) regEx += "{w+}"
-//         else regEx += chunk
-//     }
-//     regEx += '/'
-
-//     routes.push([new RegExp(regEx), callback])
-// }
-
-// addNewRoute('/home', () => { console.log('home') })
-// addNewRoute('/about', () => { console.log('about') })
-// addNewRoute('/users/{id://}', () => { console.log('users') })
-
-// const handleRequest = (req: any, res: any) => {
-//     for (const route of routes) {
-//         if(route[0].test(req.url)) return route[1](req, res);
-//     }
-// }
-
-// const server = createServer(handleRequest);
-
-// const port = 1234;
-// server.listen(port, () => { console.log(`Server started on http://localhost:${port}`) })
-// ------------------------------------------------------------------------------------------
-
 import { createServer } from "http";
 
 var routes: any = []
 
-const addNewRouter = (reqUrl: String, callback: (res:any) => void) => {
-    let routeRegex:string = "/"
-    let routeChunks:string[] = reqUrl.split('/').filter(item => item !== '')
-    for(let chunck of routeChunks){
-        if((new RegExp("{\w+}")).test(chunck)) {
-            routeRegex+= "{\w+}"
-        }else{
-            routeRegex+= "\w+"
+const addNewRouter = (reqUrl: String, callback: (res: any) => void) => {
+    reqUrl = reqUrl.split('/').filter(item => item !== '').join('/')
+
+    let routeRegex: string = ""
+    let routeChunks: string[] = reqUrl.split('/').filter(item => item !== '')
+    for (let chunck of routeChunks) {
+        if ((new RegExp(/{\w+}/)).test(chunck)) {
+            routeRegex += "([a-zA-Z0-9_-\\W])+"
+        } else {
+            routeRegex += chunck
         }
-        (chunck !== routeChunks[routeChunks.length - 1]) && (routeRegex+="/")
-        // routeRegex+="/"
+
+        if (chunck !== routeChunks[routeChunks.length - 1]) {
+            routeRegex += "\/"
+        }
     }
+
     console.log(routeRegex);
-    
+
     routes.push([
         routeRegex,
         callback
-    ]
-    )
+    ])
 }
 
 addNewRouter('/moo', (res) => res.end("moo"))
 addNewRouter('/omar', (res) => res.end("omar"))
-addNewRouter('/user/{id}', (res) => res.end("user id"))
+addNewRouter('/user/{id}/again', (res) => res.end("user id"))
 
 const handleRequest = (req: any, res: any) => {
     for (const route of routes) {
